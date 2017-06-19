990772ec9733323ad6c7e775b7211f08e3e35fa8
Simon Kofod <github@kofod.it>
2017-06-19 22:56:57 +0200

    Add console app

--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 A short demonstration of how RavenDB can be used to persist data
 
 # How it works
-- Simply reference RavenDB from NuGet.
-- Query by linq
+- Simply reference RavenDB from NuGet
+- Create a document store
+- Open session
 
 # The Niec Parts
 - No SQL!
 - No Stored Procedures!
-- No need for a DBA to make a schema change
-- Testable
+- No need for a DBA to make a schema change!
+- No need to coordinate deployments with DBA!
+- Testable!
 
 # Tricky Parts
 - Eventual concistency
@@ -17,5 +19,8 @@ A short demonstration of how RavenDB can be used to persist data
 - Number of returned documents are default 128. Max 1024. So pagination is needed
 - Max number of operations in a session is 20.
 
+# Note
+- The console app requires ravendb running locally on port 8080, and versioning enabled.
+
 # TODO
-- Revisions.
+- Test Revisions
---
+++ RavenByExample.Program/Program.cs
@@ -0,0 +1,118 @@
+﻿using Raven.Client;
+using Raven.Client.Document;
+using RavenDbByExample.Repository;
+using RavenDbByExample.Repository.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace RavenByExample.Program
+{
+    class Program
+    {
+        private static IDocumentStore _documentStore;
+        private static IPizzaRepository _pizzaRepository;
+
+        static void Main(string[] args)
+        {
+            Setup();
+
+            char key = PrintMenu();
+            while (key != '0')
+            {
+                PerformAction(key);
+                key = PrintMenu();
+            }
+
+            Teardown();
+        }
+
+        private static void PerformAction(char key)
+        {
+            switch (key)
+            {
+                case '1': 
+                    AddPizza();
+                    break;
+                case '2': 
+                    GetPizza();
+                    break;
+                case '3':
+                    GetRevisions();
+                    break;
+            }
+        }
+
+        private static void GetPizza()
+        {
+            Console.Write("\n\nGet Pizza\nName: ");
+            string name;
+            while ((name = Console.ReadLine()).Length == 0) ;
+
+            var pizza = _pizzaRepository.Get(name);
+            Console.WriteLine("{0}, {1}\n", pizza.Id, pizza.Description);
+        }
+
+        private static void GetRevisions()
+        {
+            Console.Write("\n\nGet Pizza Revisions\nName: ");
+            string name;
+            while ((name = Console.ReadLine()).Length == 0);
+
+            var revisions = _pizzaRepository.GetRevisions(name);
+            
+            foreach(var revision in revisions)
+            {
+                Console.WriteLine("{0}, {1}", revision.Id, revision.Description);
+            }
+            Console.WriteLine("\n");
+        }
+
+        private static void AddPizza()
+        {
+            Console.Write("\n\nEnter Pizza\nName: ");
+            string name;
+            while ((name = Console.ReadLine()).Length == 0) ;
+
+            Console.Write("Description: ");
+            string description;
+            while ((description = Console.ReadLine()).Length == 0) ;
+
+            var pizza = new Pizza
+            {
+                Id = name,
+                Description = description,
+                Toppings = new List<Topping> { Topping.Tomatoes, Topping.Cheese, Topping.Pepperoni },
+                Rating = new Rating
+                {
+                    Score = Score.OK,
+                    Reason = "Just for some reason",
+                }
+            };
+            _pizzaRepository.Add(pizza);
+            Console.WriteLine("Pizza Added\n");
+        }
+
+        private static char PrintMenu()
+        {
+            Console.WriteLine("Menu:\n1) Add Pizza\n2) Get pizza\n3) Get Revisions");
+            Console.Write("Select: ");
+            return Console.ReadKey().KeyChar;
+        }
+
+        private static void Setup()
+        {
+            _documentStore = new DocumentStore
+            {
+                Url = "http://localhost:8080/",
+                DefaultDatabase = "PizzaDB"
+            }.Initialize();
+
+            _pizzaRepository = new PizzaRepository(_documentStore);
+        }
+
+        private static void Teardown()
+        {
+            _documentStore.Dispose();
+        }
+    }
+}
